dedb09ffc15010c71af00fc5e497bfaf628cab14
Jake Kwak <jakekwak1129@gmail.com>
2021-03-15 16:54:59 -0400

    Edits

--- Tests_Kwak/Combining Data Sets.R
+++ Tests_Kwak/Combining Data Sets.R
@@ -1,6 +1,7 @@
-library(tidyr)
-library(dplyr)
 library(tidyverse)
+library(shiny)
+library(rgdal)
+library(dplyr)
 
 Litterfall <-
   read_csv("Z:/Virginia Tech School Work/Current Classes/Capstone/Project directory/Directory 2/EI-Capstone-21-Litter/Data/Litterfall.csv")
@@ -25,3 +26,18 @@ LitterfallLocFilt <- Litterfall %>%
 CombinedLitLoc <- merge(LitterBasketLoc, LitterfallLocFilt)
   
 #SoilRespLoc <- 
+
+#Read in geo coordinate dataset
+geo_dat <- CombinedLitLoc
+
+#Subset data to only include UTM Easting and Northing columns
+sp_dat <- geo_dat[c("stake1_utm_x", "stake1_utm_y")]
+
+#Define UTM positioning
+utm <- SpatialPoints(sp_dat, proj4string=CRS("+proj=utm +zone=19T +datum=WGS84"))
+
+#Transform UTM data to lat/long based on projection info
+CombinedLitLocLatLong <- spTransform(utm, CRS("+proj=longlat +datum=WGS84"))
+
+CombinedLitLocLatLong <- as.data.frame(CombinedLitLocLatLong)
+write_csv(CombinedLitLocLatLong, "CombinedLitLocLatLong.csv")
