5819607a6ef5a485d3208b1f7be85dce0cca8169
Jake Kwak <jakekwak1129@gmail.com>
2021-03-19 02:47:29 -0400

    Kwak Testing

--- Tests_Kwak/Combining Data Sets.R
+++ Tests_Kwak/Combining Data Sets.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(shiny)
 library(rgdal)
 library(dplyr)
+library(sp)
 
 Litterfall <-
   read_csv("Z:/Virginia Tech School Work/Current Classes/Capstone/Project directory/Directory 2/EI-Capstone-21-Litter/Data/Litterfall.csv")
@@ -16,37 +17,36 @@ Subplot_Coord <-
 LitterBasket_Coord <-
   read_csv("Z:/Virginia Tech School Work/Current Classes/Capstone/Project directory/Directory 2/EI-Capstone-21-Litter/Data/LitterBasket_Coord.csv")
 
+#converting UTM to Lat Long
+LitterUTM <- select(LitterBasket_Coord, stand, plot, basket, stake1_utm_x, stake1_utm_y)
 
+utm <- SpatialPoints(LitterUTM[c("stake1_utm_x", "stake1_utm_y")], proj4string=CRS("+proj=utm +zone=19T +datum=WGS84"))
 
+Lat_Long_Lit <- spTransform(utm, CRS("+proj=longlat +datum=WGS84"))
 
-LitterBasketLoc <- select(LitterBasket_Coord, stand, basket, stake1_utm_x, stake1_utm_y)
+Lat_Long_Lit <- as.data.frame(Lat_Long_Lit)
 
-LitterMerge <- merge(Litterfall, LitterBasketLoc)
+write_csv(Lat_Long_Lit, "Lat_Long_Lit")
 
-CombinedLitLoc <- merge(LitterBasketLoc, LitterfallLocFilt)
+Litterfall_Lat_Long <- LitterUTM %>% 
+  mutate(Lat = Lat_Long_Lit$stake1_utm_y) %>% 
+  mutate(Long = Lat_Long_Lit$stake1_utm_x)
+
+lat_long <- lat_long %>%
+  rename(Stand = Site, Basket = Stake)
+
+Litterfall_Lat_Long <- Litterfall_Lat_Long %>% 
+  rename(Stand = stand, Plot = plot, Basket = basket)
+
+LitterMerge <- merge(Litterfall, lat_long)
+
+LitterMerge <- LitterMerge%>% 
+  mutate(popup_info = paste("Stand:", Stand, "<br/>",
+                            "Plot:", Plot, "<br/>",
+                            "Basket:", Basket, "<br/>",
+                            "Year:", Year, "<br/>",
+                            "Treatment:", Treatment))
   
-#SoilRespLoc <- 
-
-#Read in geo coordinate dataset
-geo_dat <- CombinedLitLoc
-
-#Subset data to only include UTM Easting and Northing columns
-sp_dat <- geo_dat[c("stake1_utm_x", "stake1_utm_y")]
-
-#Define UTM positioning
-utm <- SpatialPoints(sp_dat, proj4string=CRS("+proj=utm +zone=19T +datum=WGS84"))
-
-#Transform UTM data to lat/long based on projection info
-CombinedLitLocLatLong <- spTransform(utm, CRS("+proj=longlat +datum=WGS84"))
-
-CombinedLitLocLatLong <- as.data.frame(CombinedLitLocLatLong)
-write_csv(CombinedLitLocLatLong, "CombinedLitLocLatLong.csv")
-
-
-
-
-
-
 
 
 
