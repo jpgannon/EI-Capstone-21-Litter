c390ffa5f958f03526c91d570db30840c26dc23f
marleyg99 <marleygilliam7@gmail.com>
2021-03-24 18:09:18 -0400

    all litterfall data plots

--- semicombined dash marley.R
+++ semicombined dash marley.R
@@ -552,7 +552,6 @@ server <- function(input, output) {
     Plotselection <- input$Plot
     
     
-    
     Litterfall %>%
       filter(Year >= min & Year <= max) %>%
       filter(Stand %in% Standselection & Treatment %in% Treatmentselection & Plot %in% Plotselection) %>%
